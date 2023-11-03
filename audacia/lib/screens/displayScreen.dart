6c1615e1ff0423d3318be812bb6b8a02ca781e60
clutchkingasiimov <saureyj43@gmail.com>
2023-11-03 16:01:01 +0100

    Added 'Stack' widget to overlay images on Card()

--- audacia/lib/screens/displayScreen.dart
+++ audacia/lib/screens/displayScreen.dart
@@ -16,46 +16,76 @@ class MainMenuPage extends StatelessWidget{
           mainAxisSpacing: 20,
           crossAxisCount: 2,
           children: const <Widget>[
-            ScreenCards('Insulin Tracker'),
-            ScreenCards('Carb Tracker'),
-            // ScreenCards('Levels Tracker'),
-            ScreenCards('Mood Tracker'),
-            ScreenCards('Calculators'),
-            ScreenCards('Medication'),
-            ScreenCards('Statistics')
-            // ScreenCards('42 Factors')
+            ScreenCards('Insulin Tracker','syringe.png'),
+            ScreenCards('Carb Tracker','food.png'),
+            ScreenCards('Calculators','calculator.png'),
+            ScreenCards('Statistics','analytics.png'),
+            ScreenCards('Medication','medication.png'),
+            ScreenCards('42 Factors','levels.png')
           ]
-        )
-    );
+        ),
+        bottomNavigationBar: NavigationBar(
+          destinations: const [
+            NavigationDestination(icon: Icon(Icons.calendar_month), 
+            label: 'Calendar'), 
+            NavigationDestination(icon: Icon(Icons.home), 
+            label: 'Home'),
+            NavigationDestination(icon: Icon(Icons.settings), 
+            label: 'Settings'),
+          ],
+          indicatorColor: lightColorScheme.secondaryContainer,
+          selectedIndex: 1,),
+        );
   }
 }
 
+//ScreenCards widget
 class ScreenCards extends StatelessWidget{
   final String buttonName;
+  final String imageName;
 
-  const ScreenCards(this.buttonName);
+  //The image dimensions for the Card display
+  static const double imageDim = 80.0;
+  static const double cardHeight = 144;
+  static const double cardWidth = 155;
+
+  const ScreenCards(this.buttonName, this.imageName, {super.key});
 
   @override 
   Widget build(BuildContext context){
-    //Use cards to create 8 different buttons to access
-    //8 different screens
+    //Use cards to create 6 different buttons to access
+    //6 different screens
     return Card(
       clipBehavior: Clip.hardEdge,
+      color: lightColorScheme.primaryContainer,
       //Card elevation height to give shadow depth
-      elevation: 4.0,
+      elevation: 2.0,
       child: InkWell(splashColor: Colors.blue.withAlpha(40),
+      //OnTap action
       onTap: () {},
-      child: SizedBox(
-        width: 155,
-        height: 144,
-        //
+      //Use a Stack widget to add multiple children widgets
+      //This was a way to add images on the cards
+      child: Stack(children: <Widget>[
+        SizedBox(width: cardWidth, 
+        height: cardHeight,
         child: Align(
-          alignment: const Alignment(0.00, 0.70),
+          alignment: const Alignment(0.00, 1.00),
           child: Text(buttonName,
-          style: const TextStyle(fontSize: 20)),
+          style: TextStyle(fontSize: 18,
+          color: lightColorScheme.onPrimaryContainer)),
+          ),
         ),
+        Positioned(
+          left: 0.0,
+          right: 0.0,
+          bottom: 50.0, 
+          child: Image.asset('lib/images/$imageName',
+            height: imageDim,
+            width: imageDim),
+        )
+      ],
       ),
-      )
-    );
+    ),
+  );
   }
 }
